7f05c51de4e9b8ef946b7be48d7d82a7ceda30d9
Monroe Lee King Jr <monroelkjr@gmail.com>
2026-09-12 04:13:04 -0700

    Add J2534 filter API types

--- native/j2534_proxy/j2534_api.h
+++ native/j2534_proxy/j2534_api.h
@@ -4,6 +4,16 @@
 
 namespace atlas_j2534 {
 
+struct PASSTHRU_MSG {
+  unsigned long ProtocolID;
+  unsigned long RxStatus;
+  unsigned long TxFlags;
+  unsigned long Timestamp;
+  unsigned long DataSize;
+  unsigned long ExtraDataIndex;
+  unsigned char Data[4128];
+};
+
 using PassThruOpenFn = long(WINAPI*)(void* pName, unsigned long* pDeviceID);
 using PassThruCloseFn = long(WINAPI*)(unsigned long DeviceID);
 using PassThruConnectFn = long(WINAPI*)(
@@ -13,6 +23,16 @@ using PassThruConnectFn = long(WINAPI*)(
     unsigned long BaudRate,
     unsigned long* pChannelID);
 using PassThruDisconnectFn = long(WINAPI*)(unsigned long ChannelID);
+using PassThruStartMsgFilterFn = long(WINAPI*)(
+    unsigned long ChannelID,
+    unsigned long FilterType,
+    PASSTHRU_MSG* pMaskMsg,
+    PASSTHRU_MSG* pPatternMsg,
+    PASSTHRU_MSG* pFlowControlMsg,
+    unsigned long* pFilterID);
+using PassThruStopMsgFilterFn = long(WINAPI*)(
+    unsigned long ChannelID,
+    unsigned long FilterID);
 using PassThruReadVersionFn = long(WINAPI*)(
     unsigned long DeviceID,
     char* pFirmwareVersion,
@@ -22,10 +42,19 @@ using PassThruGetLastErrorFn = long(WINAPI*)(char* pErrorDescription);
 
 constexpr unsigned long PROTOCOL_ISO15765 = 0x06;
 
+constexpr unsigned long PASS_FILTER = 0x01;
+constexpr unsigned long BLOCK_FILTER = 0x02;
+constexpr unsigned long FLOW_CONTROL_FILTER = 0x03;
+
 constexpr long STATUS_NOERROR = 0x00;
+constexpr long ERR_NOT_SUPPORTED = 0x01;
 constexpr long ERR_INVALID_CHANNEL_ID = 0x02;
 constexpr long ERR_INVALID_PROTOCOL_ID = 0x03;
+constexpr long ERR_NULL_PARAMETER = 0x04;
+constexpr long ERR_INVALID_FLAGS = 0x06;
 constexpr long ERR_FAILED = 0x07;
+constexpr long ERR_INVALID_MSG = 0x0A;
+constexpr long ERR_INVALID_FILTER_ID = 0x16;
 constexpr long ERR_INVALID_BAUDRATE = 0x19;
 constexpr long ERR_INVALID_DEVICE_ID = 0x1A;
 
