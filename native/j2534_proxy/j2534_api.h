69003ae4c56662e3e1d9a20b62837cc217ef054f
Monroe Lee King Jr <monroelkjr@gmail.com>
2026-09-12 03:23:51 -0700

    Add J2534 connect and disconnect API types

--- native/j2534_proxy/j2534_api.h
+++ native/j2534_proxy/j2534_api.h
@@ -6,6 +6,13 @@ namespace atlas_j2534 {
 
 using PassThruOpenFn = long(WINAPI*)(void* pName, unsigned long* pDeviceID);
 using PassThruCloseFn = long(WINAPI*)(unsigned long DeviceID);
+using PassThruConnectFn = long(WINAPI*)(
+    unsigned long DeviceID,
+    unsigned long ProtocolID,
+    unsigned long Flags,
+    unsigned long BaudRate,
+    unsigned long* pChannelID);
+using PassThruDisconnectFn = long(WINAPI*)(unsigned long ChannelID);
 using PassThruReadVersionFn = long(WINAPI*)(
     unsigned long DeviceID,
     char* pFirmwareVersion,
@@ -13,8 +20,13 @@ using PassThruReadVersionFn = long(WINAPI*)(
     char* pApiVersion);
 using PassThruGetLastErrorFn = long(WINAPI*)(char* pErrorDescription);
 
+constexpr unsigned long PROTOCOL_ISO15765 = 0x06;
+
 constexpr long STATUS_NOERROR = 0x00;
+constexpr long ERR_INVALID_CHANNEL_ID = 0x02;
+constexpr long ERR_INVALID_PROTOCOL_ID = 0x03;
 constexpr long ERR_FAILED = 0x07;
+constexpr long ERR_INVALID_BAUDRATE = 0x19;
 constexpr long ERR_INVALID_DEVICE_ID = 0x1A;
 
 }  // namespace atlas_j2534
