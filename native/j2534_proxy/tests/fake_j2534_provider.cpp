e66f8664e0658c4e92c388d13d537b08bc3ad5a7
Monroe Lee King Jr <monroelkjr@gmail.com>
2026-09-12 03:24:04 -0700

    Add fake J2534 connection lifecycle

--- native/j2534_proxy/tests/fake_j2534_provider.cpp
+++ native/j2534_proxy/tests/fake_j2534_provider.cpp
@@ -7,6 +7,8 @@
 
 namespace {
 constexpr unsigned long kDeviceId = 0x1234;
+constexpr unsigned long kChannelId = 0x2345;
+constexpr unsigned long kBaudRate = 500000;
 }
 
 extern "C" __declspec(dllexport) long WINAPI PassThruOpen(
@@ -20,6 +22,25 @@ extern "C" __declspec(dllexport) long WINAPI PassThruClose(
     unsigned long DeviceID) {
   return DeviceID == kDeviceId ? atlas_j2534::STATUS_NOERROR
                                : atlas_j2534::ERR_INVALID_DEVICE_ID;
+}
+
+extern "C" __declspec(dllexport) long WINAPI PassThruConnect(
+    unsigned long DeviceID, unsigned long ProtocolID, unsigned long Flags,
+    unsigned long BaudRate, unsigned long* pChannelID) {
+  if (DeviceID != kDeviceId) return atlas_j2534::ERR_INVALID_DEVICE_ID;
+  if (ProtocolID != atlas_j2534::PROTOCOL_ISO15765) {
+    return atlas_j2534::ERR_INVALID_PROTOCOL_ID;
+  }
+  if (BaudRate != kBaudRate) return atlas_j2534::ERR_INVALID_BAUDRATE;
+  if (Flags != 0 || pChannelID == nullptr) return atlas_j2534::ERR_FAILED;
+  *pChannelID = kChannelId;
+  return atlas_j2534::STATUS_NOERROR;
+}
+
+extern "C" __declspec(dllexport) long WINAPI PassThruDisconnect(
+    unsigned long ChannelID) {
+  return ChannelID == kChannelId ? atlas_j2534::STATUS_NOERROR
+                                 : atlas_j2534::ERR_INVALID_CHANNEL_ID;
 }
 
 extern "C" __declspec(dllexport) long WINAPI PassThruReadVersion(
